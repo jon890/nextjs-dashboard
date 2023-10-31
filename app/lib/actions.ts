625a38adfea7c1551209905dc186804a8c186617
BiFoS <jon89071@gmail.com>
2023-10-31 11:38:10 +0900

    :passport_control: Authentication with NextAuth

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { signIn } from "@/auth";
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
@@ -95,8 +96,6 @@ export async function updateInvoice(formData: FormData) {
 const DeleteInvoice = InvoiceSchema.pick({ id: true });
 
 export async function deleteInvoice(formData: FormData) {
-  throw new Error("Failed to Delete Invoice");
-
   const { id } = DeleteInvoice.parse({
     id: formData.get("id"),
   });
@@ -107,5 +106,19 @@ export async function deleteInvoice(formData: FormData) {
     return { message: "Deleted Invoice." };
   } catch (error) {
     return { message: "Database Error: Failed to Delete Invoice." };
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", Object.fromEntries(formData));
+  } catch (error) {
+    if ((error as Error).message.includes("CredentialsSignin")) {
+      return "CredentialSignin";
+    }
+    throw error;
   }
 }
